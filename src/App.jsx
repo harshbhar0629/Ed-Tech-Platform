7080c667347ed893a87a052702827247cc4f4fe1
harshbhar0629 <harshbhar.0629@gmail.com>
2024-09-03 22:49:11 +0530

    Adding code for privateRoute and error page

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/common/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import OpenRoute from "./components/core/Auth/OpenRoute"
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
+
 	return (
 		<div className=" w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
 			<Navbar />
@@ -16,11 +19,26 @@ function App() {
 					path="/"
 					element={<Home />}></Route>
 				<Route
-					path="login"
-					element={<Login />}></Route>
+					path="/login"
+					element={
+						<OpenRoute>
+							<Login />
+						</OpenRoute>
+					}></Route>
 				<Route
-					path="signup"
-					element={<Signup />}></Route>
+					path="/signup"
+					element={
+						<OpenRoute>
+							<Signup />
+						</OpenRoute>
+					}></Route>
+				<Route
+					path="forgot-password"
+					element={
+						<OpenRoute>
+							<ForgotPassword />
+						</OpenRoute>
+					}></Route>
 			</Routes>
 		</div>
 	);
