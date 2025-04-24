f3b740bad6533498b176bab2a52742614bd69eea
harshbhar0629 <harshbhar.0629@gmail.com>
2025-04-24 08:34:44 +0530

    fix

--- server/controllers/CourseProgress.js
+++ server/controllers/CourseProgress.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose");
 const Section = require("../models/Section");
-const SubSection = require("../models/Subsection");
+const SubSection = require("../models/SubSection");
 const CourseProgress = require("../models/CourseProgress");
 const Course = require("../models/Course");
 
--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -2,7 +2,7 @@
 
 const Section = require("../models/Section");
 const Course = require("../models/Course");
-const SubSection = require("../models/Subsection");
+const SubSection = require("../models/SubSection");
 
 // CREATE a new section
 exports.createSection = async (req, res) => {
